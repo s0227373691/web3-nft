3aaa23aa1bfc8270f6a8537cce4e142117b3d869
Elmer <s0227373691@gms.tku.edu.tw>
2024-01-15 16:19:44 +0800

    add upload metadata

---
+++ src/pages/api/metadata.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const pinataSDK = require("@pinata/sdk");
+const pinata = new pinataSDK({ pinataJWTKey: process.env.PINATA_JWT });
+
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    try {
+      const metadata = req.body;
+      const json = JSON.parse(metadata);
+      const response = await pinata.pinJSONToIPFS(json);
+
+      const { IpfsHash } = response;
+
+      return res.send(IpfsHash);
+    } catch (e) {
+      console.log(e);
+      res.status(500).send("Server Error");
+    }
+  } else if (req.method === "GET") {
+    try {
+      const response = await pinata.pinList(
+        { pinataJWTKey: process.env.PINATA_JWT },
+        {
+          pageLimit: 1,
+        }
+      );
+      res.json(response.rows[0]);
+    } catch (e) {
+      console.log(e);
+      res.status(500).send("Server Error");
+    }
+  }
+}
